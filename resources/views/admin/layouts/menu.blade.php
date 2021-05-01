9f56833f96c963f25870d1fbad394737474e1eaa
ramincse <ramin.cse2009@gmail.com>
2021-05-01 13:34:39 +0600

    Update Theme option

--- resources/views/admin/layouts/menu.blade.php
+++ resources/views/admin/layouts/menu.blade.php
@@ -9,7 +9,7 @@
                         <a href="{{ route('admin.dashboard') }}"><i class="fe fe-home"></i> <span>Dashboard</span></a>
                     </li>
                     <li class="{{ (Route::currentRouteName() == 'portfolio.index') ? 'active' : '' }}">
-                        <a href="{{ route('portfolio.index') }}"><i class="fe fe-layout"></i> <span>Portfolio</span></a>
+                        <a href="{{ route('portfolio.index') }}"><i class="fa fa-file-image-o" aria-hidden="true"></i> <span>Portfolio</span></a>
                     </li>
 
                     <li class="{{ (Route::currentRouteName() == 'service.index') ? 'active' : '' }}">
@@ -17,15 +17,15 @@
                     </li>
 
                     <li class="{{ (Route::currentRouteName() == 'fsservice.index') ? 'active' : '' }}">
-                        <a href="{{ route('fsservice.index') }}"><i class="fe fe-user"></i> <span>Security Services</span></a>
+                        <a href="{{ route('fsservice.index') }}"><i class="fa fa-user-secret" aria-hidden="true"></i> <span>Security Services</span></a>
                     </li>
 
                     <li class="{{ (Route::currentRouteName() == 'testimonial.index') ? 'active' : '' }}">
-                        <a href="{{ route('testimonial.index') }}"><i class="fe fe-star-o"></i> <span>Testimonial</span></a>
+                        <a href="{{ route('testimonial.index') }}"><i class="fa fa-commenting-o" aria-hidden="true"></i> <span>Testimonial</span></a>
                     </li>
 
                     <li class="submenu">
-                        <a href="#"><i class="fe fe-document"></i> <span>About Us</span> <span class="menu-arrow"></span></a>
+                        <a href="#"><i class="fa fa-info" aria-hidden="true"></i> <span>About Us</span> <span class="menu-arrow"></span></a>
                         <ul style="display: none;">
                             <li class="{{ (Route::currentRouteName() == 'about-management.index') ? 'ok' : '' }}"><a href="{{ route('about-management.index') }}">Management</a></li>
                             <li class="{{ (Route::currentRouteName() == 'about-vice.index') ? 'ok' : '' }}"><a href="{{ route('about-vice.index') }}">Director Message</a></li>
@@ -38,29 +38,21 @@
                     </li>
 
                     <li class="{{ (Route::currentRouteName() == 'gallery.index') ? 'active' : '' }}">
-                        <a href="{{ route('gallery.index') }}"><i class="fe fe-vector"></i> <span>Gallery</span></a>
+                        <a href="{{ route('gallery.index') }}"><i class="fa fa-picture-o" aria-hidden="true"></i> <span>Gallery</span></a>
                     </li>
                     <li class="submenu">
-                        <a href="#"><i class="fe fe-document"></i> <span>Our Services</span> <span class="menu-arrow"></span></a>
+                        <a href="#"><i class="fa fa-server" aria-hidden="true"></i> <span>Our Services</span> <span class="menu-arrow"></span></a>
                         <ul style="display: none;">
                             <li class="{{ (Route::currentRouteName() == 'top.service') ? 'ok' : '' }}"><a href="{{ route('top.service') }}">Top Service</a></li>
-                            <li class="{{ (Route::currentRouteName() == 'accessory.index') ? 'ok' : '' }}"><a href="{{ route('accessory.index') }}">Invoice Reports</a></li>
+                            <li class="{{ (Route::currentRouteName() == 'accessory.index') ? 'ok' : '' }}"><a href="{{ route('accessory.index') }}">Accessories</a></li>
                         </ul>
                     </li>
                     <li class="submenu">
-                        <a href="#"><i class="fe fe-document"></i> <span>Contact</span> <span class="menu-arrow"></span></a>
+                        <a href="#"><i class="fa fa-compress" aria-hidden="true"></i> <span>Contact</span> <span class="menu-arrow"></span></a>
                         <ul style="display: none;">
                             <li class="{{ (Route::currentRouteName() == 'contact.us') ? 'ok' : '' }}"><a href="{{ route('contact.us') }}">Office Address</a></li>
                         </ul>
                     </li>
-
-                    <!--
-                    <li class="menu-title">
-                        <span>Pages</span>
-                    </li>
-                    <li>
-                        <a href="profile.html"><i class="fe fe-user-plus"></i> <span>Profile</span></a>
-                    </li> -->
                     <li class="submenu">
                         <a href="#"><i class="fe fe-vector"></i> <span> Settings </span> <span class="menu-arrow"></span></a>
                         <ul style="display: none;">
@@ -68,62 +60,7 @@
                             <li class="{{ (Route::currentRouteName() == 'theme.setting') ? 'ok' : '' }}"><a href="{{ route('theme.setting') }}">Theme Option</a></li>
                         </ul>
                     </li>
-                    <!-- <li class="submenu">
-                        <a href="#"><i class="fe fe-warning"></i> <span> Error Pages </span> <span class="menu-arrow"></span></a>
-                        <ul style="display: none;">
-                            <li><a href="error-404.html">404 Error </a></li>
-                            <li><a href="error-500.html">500 Error </a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="blank-page.html"><i class="fe fe-file"></i> <span>Blank Page</span></a>
-                    </li>
-                    <li class="menu-title">
-                        <span>UI Interface</span>
-                    </li>
-                    <li>
-                        <a href="components.html"><i class="fe fe-vector"></i> <span>Components</span></a>
-                    </li>
-                    <li class="submenu">
-                        <a href="#"><i class="fe fe-layout"></i> <span> Forms </span> <span class="menu-arrow"></span></a>
-                        <ul style="display: none;">
-                            <li><a href="form-basic-inputs.html">Basic Inputs </a></li>
-                            <li><a href="form-input-groups.html">Input Groups </a></li>
-                            <li><a href="form-horizontal.html">Horizontal Form </a></li>
-                            <li><a href="form-vertical.html"> Vertical Form </a></li>
-                            <li><a href="form-mask.html"> Form Mask </a></li>
-                            <li><a href="form-validation.html"> Form Validation </a></li>
-                        </ul>
-                    </li>
-                    <li class="submenu">
-                        <a href="#"><i class="fe fe-table"></i> <span> Tables </span> <span class="menu-arrow"></span></a>
-                        <ul style="display: none;">
-                            <li><a href="tables-basic.html">Basic Tables </a></li>
-                            <li><a href="data-tables.html">Data Table </a></li>
-                        </ul>
-                    </li>
-                    <li class="submenu">
-                        <a href="javascript:void(0);"><i class="fe fe-code"></i> <span>Multi Level</span> <span class="menu-arrow"></span></a>
-                        <ul style="display: none;">
-                            <li class="submenu">
-                                <a href="javascript:void(0);"> <span>Level 1</span> <span class="menu-arrow"></span></a>
-                                <ul style="display: none;">
-                                    <li><a href="javascript:void(0);"><span>Level 2</span></a></li>
-                                    <li class="submenu">
-                                        <a href="javascript:void(0);"> <span> Level 2</span> <span class="menu-arrow"></span></a>
-                                        <ul style="display: none;">
-                                            <li><a href="javascript:void(0);">Level 3</a></li>
-                                            <li><a href="javascript:void(0);">Level 3</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="javascript:void(0);"> <span>Level 2</span></a></li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0);"> <span>Level 1</span></a>
-                            </li>
-                        </ul>
-                    </li> -->
+                    
                 </ul>
             </div>
         </div>
